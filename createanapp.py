c4234c7a96349f25543d4edd60bbd6c22968fa06
Santoesteban <127811984+Santoesteban@users.noreply.github.com>
2023-03-15 22:01:22 -0600

    Add files via upload

--- createanapp.py
+++ createanapp.py
@@ -5,52 +5,34 @@ Created on Tue Mar  7 08:12:38 2023
 @author: Hugo
 """
 
+
 import streamlit as st
 import pandas as pd
-import numpy as np
-import joblib
-
 import os
 import tarfile
 import urllib.request
-#from combined_attributes_adder import CombineAttributeAdder
-
-
-
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
 from sklearn.base import BaseEstimator, TransformerMixin
-
-# column index
-rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
-
-
-class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
-        self.add_bedrooms_per_room = add_bedrooms_per_room
-    def fit(self, X, y=None):
-        return self  # nothing else to do
-    def transform(self, X):
-        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
-        population_per_household = X[:, population_ix] / X[:, households_ix]
-        if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, population_per_household,
-                         bedrooms_per_room]
-        else:
-            return np.c_[X, rooms_per_household, population_per_household]
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
 
 
+# In[83]:
 
-
-
-#https://github.com/Santoesteban/Santoesteban.github.io.git
 
 def predict(data, model_name):
     model = joblib.load(f'{model_name}')
     pipeline= joblib.load('pipeline.sav')
     transformed_data = pipeline.transform(data)
     return model.predict(transformed_data)
-# Cargar el modelo
 
+
+# In[84]:
 
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
@@ -71,106 +53,140 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
+# In[92]:
+
+
+housing = load_housing_data()
+
+
+# In[100]:
+
+
+imputer = SimpleImputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+
+
+# In[102]:
+
+
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing.index)
+
+
+# In[101]:
+
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True): 
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self 
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+
+# In[94]:
+
+
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+
+# In[95]:
+
+
 header = st.container()
 dataset = st.container()
 inputs = st.container()
 modelTraining = st.container()
 
 
+# In[96]:
+
+
 with header:
-    st.title('Precio de las casas')
-    
+    st.title('Housing Project Prediction')
+
+
+# In[97]:
+
 
 with dataset:
     st.header('Housing Dataset')
     st.text('Se muestran la información que existe en nuestra base de datos')
     housing = load_housing_data()
-    st.write(housing)
+    st.write(housing.head())
+
+
+# In[98]:
 
 
 with inputs:
     st.header('Inputs del modelo')
     st.text('Selecciona los inputs para poder predecir el precio de la casa de tus sueños')
     
-    sel_col, disp_col= st.columns(2)
-    
-    longitude = st.number_input('longitud', min_value=-200, max_value= 0, value= -100, step=1)
-    latitude = st.number_input('latitud', min_value=0, max_value= 60, value= 30, step=1)
-    housing_median_age = st.number_input('hausing_median_age', min_value=0, max_value= 100, value= 20, step=1)
-    location = sel_col.selectbox('¿En qué zona te gustaría?', options=["ISLAND","NEAR BAY", "NEAR OCEAN", "INLAND", "<1H OCEAN"], index = 0)
-    total_rooms = st.number_input('Número de cuartos', min_value=1, max_value= 10, value= 1, step=1)
-    total_bedrooms = st.number_input('Número de baños', min_value=1, max_value= 10, value= 1, step=1)
-    population = st.number_input('población', min_value=1, max_value= 4000, value= 500, step=1)
-    households = st.number_input('households', min_value=1, max_value= 2000, value= 500, step=1)
-    median_income = st.number_input('medianincome', min_value=1, max_value= 2000, value= 500, step=1)
-    
-    data = [longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households,median_income]
+    sel_col1, sel_col2= st.columns(2)
+    longitude = sel_col1.number_input('Latitud del lugar', value=-124.35)
+    latitude = sel_col1.number_input('Longitud del lugar', value=32.54)
+    housing_median_age = sel_col1.slider('Años promedios de la casa', min_value=1, max_value= 52, value= 1, step=1)
+    total_rooms = sel_col1.slider('Total de cuartos', min_value=2, max_value= 39320, value= 2, step=1)
+    total_bedrooms = sel_col1.slider('Total de baños', min_value=1, max_value= 6445, value= 1, step=1)
+    population = sel_col2.slider('Poblacion total', min_value=3, max_value= 35682, value= 3, step=1)
+    households = sel_col2.slider('Tamaño de personas viviendo en la casa', min_value=1, max_value= 6082, value= 1, step=1)
+    median_income = sel_col2.number_input('Ingreso medios', value=0.4999)
+    ocean_proximity = sel_col2.selectbox('¿En qué zona te gustaría?', ['ISLAND','NEAR BAY', 'NEAR OCEAN', 'INLAND', '<1H OCEAN'], index = 0)
+    model = sel_col2.selectbox('¿Qué tipo de modelo de Machine Learning quieeras usar para tu predicción?', ['Linear Regression','Decision Tree', 'Random Forest'], index = 0)
     
     
-    data = pd.DataFrame(data)
-
-    option = st.selectbox(
-        '¿Qué método quieres usar?',
-        ('Regresión Lineal', 'Random forest', 'decision tree'))
 
 
+# In[99]:
 
 
-
-    if option == 'Regresión Lineal':
-        st.write('You selected:', option)
-        predict(data, 'l_regression.sav')
-    if option == 'Random forest':
-        st.write('You selected:', option)
-        predict(data, 'ramf_regression.sav')
-    if option == 'decision tree':
-        st.write('You selected:', option)
-        predict(data, 'dTree_regression.sav')
-
-
-
-
-
-
-"""data es el conjunto de datos que se van a escribir en el programa
-
-# Crear un input para ingresar un número
-valor = st.number_input('Ingrese un número', min_value=0, max_value=100, value=50)
-
-# Mostrar el valor ingresado
-st.write('El número ingresado es:', valor)
-
-data = pd.DataFrame(
-    [
-       {valor, "1": 7.5, "2": "430000"},
-
-   ]
-)
-edited_df = st.experimental_data_editor(data)
-
-
-favorite_command = edited_df.loc[edited_df["median_income"].idxmax()]["median_house_value"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
-
-st.write(data)
-
-option = st.selectbox(
-    '¿Qué método quieres usar?',
-    ('Regresión Lineal', 'Random forest', 'decision tree'))
+with modelTraining:
+    st.header('Resultados del Modelo de ML')
+    if st.button ('Toca para predecir el precio de la casa'):
+        data = pd.DataFrame({
+            'longitude' : [longitude],
+            'latitude' : [latitude],
+            'housing_median_age' : [housing_median_age],
+            'total_rooms' : [total_rooms],
+            'total_bedrooms': [total_bedrooms],
+            'population' : [population],
+            'households': [households],
+            'median_income' : [median_income],
+            'ocean_proximity' : [ocean_proximity]
+            })
+        if model == 'Linear Regression':
+            result = predict(data, 'lin_regression.sav')
+        elif model == 'Decision Tree':
+            result = predict(data, 'tree_regression.sav')
+        elif model == 'Random Forest':
+            result = predict(data, 'forest_regression.sav')
+    
+        st.text(f'El precio de la casa es de: ${result[0]}')
+  
+  
+  
 
 
 
-
-
-if option == 'Regresión Lineal':
-    st.write('You selected:', option)
-    predict(data, 'l_regression.sav')
-    print("random")
-if option == 'Random forest':
-    st.write('You selected:', option)
-    print("random")
-if option == 'decision tree':
-    st.write('You selected:', option)
-    print("random")
-
-"""
